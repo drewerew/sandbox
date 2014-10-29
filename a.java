d51f5f989ef01fd8a218d91ac0e406bb29d50c7a
kryan <Kevin.Ryan@ingramcontent.com>
2014-10-29 10:58:48 -0500

    only top hunk of p from 1

--- a.java
+++ a.java
@@ -2,7 +2,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeMap;
-resolving conflict o and n, from 2
+change p from 1
 
 public class AccountInformationAction extends BaseAction {
 
